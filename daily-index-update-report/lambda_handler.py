60498428e4c7d3391764eceafc25e97577b7e8d8
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-12-21 00:20:18 -0500

    updated code due to changes in yfinance and finsim

--- daily-index-update-report/lambda_handler.py
+++ daily-index-update-report/lambda_handler.py
@@ -6,7 +6,7 @@ import logging
 from finsim.data import get_yahoofinance_data
 
 
-indices = {
+default_indices = {
     '^GSPC': 'S&P 500',
     '^IXIC': 'NASDAQ Composite',
     '^DJI': 'Dow-Jones Industrial Average',
@@ -20,12 +20,12 @@ def get_recent_indices(index, numdays=30, mockedtoday=None):
     begindate = datetime.strftime(date.today()-timedelta(days=numdays), '%Y-%m-%d') if mockedtoday is None else datetime.strftime(datetime.strptime(todaystr, '%Y-%m-%d')-timedelta(days=numdays), '%Y-%m-%d')
     df = get_yahoofinance_data(index, begindate, todaystr)
     try:
-        todayclosing = df.loc[todaystr, 'Close']
+        todayclosing = df[df['Date'] == todaystr]['Close'].to_numpy()[0]
     except KeyError:
         return {}
 
     previousdaystr = datetime.strftime(df.iloc[-2, :].TimeStamp, '%Y-%m-%d')
-    previousdayclosing = df.loc[previousdaystr, 'Close']
+    previousdayclosing = df[df['Date'] == previousdaystr]['Close'].to_numpy()[0]
     return {
         'today': {'date': todaystr, 'close': todayclosing},
         'previousday': {'date': previousdaystr, 'close': previousdayclosing}
@@ -37,13 +37,12 @@ def lambda_handler(event, context):
     query = json.loads(event['body'])
     logging.info(query)
     print(query)
-    if 'mockedtoday' in query:
-        mockedtoday = query['mockedtoday']
-    else:
-        mockedtoday = None
+    mockedtoday = query.get('mockedtoday')
+    indices = query.get('indices_to_show', default_indices)
 
     report_info = []
     for idxsymbol, idxname in indices.items():
+        print('{}: {}'.format(idxsymbol, idxname))
         report_info.append({
             'index': idxsymbol,
             'name': idxname,
